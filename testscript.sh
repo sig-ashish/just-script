953752b7f0900f5b0303378c603b9659dab7c9e7
Ashish Yadav <95202504+sig-ashish@users.noreply.github.com>
2021-12-30 12:52:42 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -2,7 +2,6 @@
 
 FIRSTNUMBER=12
 SECONDNUMBER=24 
-echo "This is just a demo for jenkins"
 echo "First number is $FIRSTNUMBER"
 echo "Second number is $SECONDNUMBER"
 echo `expr $FIRSTNUMBER + $SECONDNUMBER`
