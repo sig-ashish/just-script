feb3a023ee854f7890ae8096015928bf0219263a
Ashish yadav <ashishyadav@sigmoidanalytics.com>
2022-01-05 13:26:23 +0530

    Revert "this is one last change- hi just going "
    This reverts commit ab31ff43964b79728be994c2a1c6ccc3b16a5627.

--- testscript.sh
+++ testscript.sh
@@ -4,5 +4,6 @@ FIRSTNUMBER=12
 SECONDNUMBER=24 
 echo `expr $FIRSTNUMBER + $SECONDNUMBER`
 echo "Now everything is changed for sake"
-echo "Please this is just one last change"
+echo "Ashish has added one more line"
+
 
