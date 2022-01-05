74a77c2832eeffba440ccd8d7eac3c491fd832a6
Ashish yadav <ashishyadav@sigmoidanalytics.com>
2022-01-05 14:08:14 +0530

    this one brings more changes

--- testscript.sh
+++ testscript.sh
@@ -4,9 +4,3 @@ FIRSTNUMBER=12
 SECONDNUMBER=24 
 echo `expr $FIRSTNUMBER + $SECONDNUMBER`
 echo "hi everyone this is just a last change"
-echo "hi"
-echo "hello"
-echo "making changes for jenkins"
-echo "one last more change"
-echo "one"
-echo "more"
