65ea13cdfe8911a8b1b3947105f85ac1d8b37556
Ashish yadav <ashishyadav@sigmoidanalytics.com>
2022-01-05 13:14:23 +0530

    everyting is changed now

--- testscript.sh
+++ testscript.sh
@@ -3,7 +3,6 @@
 FIRSTNUMBER=12
 SECONDNUMBER=24 
 echo `expr $FIRSTNUMBER + $SECONDNUMBER`
-echo "This script is changed now"
-echo "This is little bit changed now"
+echo "Now everything is changed for sake"
 
 
